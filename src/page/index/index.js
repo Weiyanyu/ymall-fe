d52f6a6b1c62d64264cbd3745da90a6e0423585d
Weiyanyu <chinayeonon@gmail.com>
2017-10-19 19:56:16 +0800

    完成首页的开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,10 +2,27 @@
 
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('./index.css');
+require('util/slider/index.js')
 var navSide = require('page/common/nav-side/index.js');
-navSide.init({
-    name : 'user-center'
-});
 var _ym = require('util/ym.js');
+var templateBanner = require('./banner.string')
+
+$(function() {
+    //渲染banner HTML
+    var bannerHTML = _ym.renderHTML(templateBanner);
+    $('.banner-con').html(bannerHTML)
+    //初始化banner
+    var $slider = $('.banner').unslider({
+        dots: true,
+    });
+
+    //事件绑定
+    $('.banner-con .banner-arrow').click(function() {
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
+    
+});
 
  
